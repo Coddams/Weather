b88741e5ee3f735d929bb6ae207c0d9d8beada68
Kiddams Joshua <135136453+Coddams@users.noreply.github.com>
2024-01-24 08:29:24 +0100

    Add files via upload

---
+++ Portfolio project/index.js
@@ -0,0 +1,7 @@
+const servicesData = [
+    {
+        title: "Frontend Development",
+        catefory: "frontend",
+        description: ['As a full-stack developer, I excel in crafting dynamic and user-centric frontend experiences. Leveraging my proficiency in languages such as HTML, CSS, and JavaScript, I ensure that the user interface is not only visually appealing but also intuitive and responsive. From conceptualizing the user experience (UX) design to implementing interactive elements and seamless navigation, I prioritize creating a compelling frontend that enhances user engagement. Through my expertise in popular frontend frameworks like React or Angular, I deliver scalable solutions that meet both aesthetic and functional requirements, providing users with a smooth and enjoyable interaction with the web application.']
+    }
+]
